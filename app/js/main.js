5bd97505e9a6f52e84be3659e4508a76216e8627
amera7md <amera7md@gmail.com>
2016-08-28 19:48:04 +0300

    change design fo the website

--- app/js/main.js
+++ app/js/main.js
@@ -8,19 +8,8 @@
         console.log("x" +x);
         console.log("y" +y);
     });
-var skillsNotRenderd=true; ; 
-   var loadingScreen = pleaseWait({
-                logo: "../img/loading.png",
-                backgroundColor: '#b07bb1',
-                loadingHtml: "<div class='row animation-container'> <div class='col-xs-2 col-xs-offset-5 animation-parent '> <div class=' animation blue'></div><div class=' animation red'></div><div class=' animation green'></div></div></div> "
-               });
- 
-
-setTimeout(function(){
-loadingScreen.finish();
-},3000);
-
-new WOW().init();
+ window.onload =function(){
+  //animation for the three balls 
 var ani= anime({
   targets: '.animation',
  translateY: {
@@ -36,6 +25,137 @@ var ani= anime({
   duration: 500 
 });
  
+   var waves = new SineWaves({
+  // Canvas Element
+  el: document.getElementById('waves'),
+
+  // General speed of entire wave system
+  speed: 8,
+   width: function() {
+        return $(window).width();
+      },
+      
+      height: function() {
+        return $(window).height();
+      },
+  // How many degress should we rotate all of the waves
+  rotate: 0,
+
+  // Ease function from left to right
+  ease: 'Linear',
+
+  // Specific how much the width of the canvas the waves should be
+  // This can either be a number or a percent
+  waveWidth: '95%',
+
+  // An array of wave options
+  waves: [{}],
+set1: [
+        {
+          timeModifier: 4,
+          lineWidth: 1,
+          amplitude: -25,
+          wavelength: 25
+        },
+        {
+          timeModifier: 2,
+          lineWidth: 1,
+          amplitude: -50,
+          wavelength: 50
+        },
+        {
+          timeModifier: 1,
+          lineWidth: 1,
+          amplitude: 100,
+          wavelength: 100
+        },
+        {
+          timeModifier: 0.5,
+          lineWidth: 1,
+          amplitude: -200,
+          wavelength: 200
+        },
+        {
+          timeModifier: 0.25,
+          lineWidth: 1,
+          amplitude: -300,
+          wavelength: 320
+        }
+      ],
+      
+      set2: [
+        {
+          lineWidth: 3,
+          amplitude: 150,
+          wavelength: 200,
+          segmentLength: 20
+        },
+        {
+          lineWidth: 2,
+          amplitude: 150,
+          wavelength: 100
+        },
+        {
+          lineWidth: 1,
+          amplitude: -150,
+          wavelength: 50,
+          segmentLength: 10
+        },
+        {
+          lineWidth: 0.5,
+          amplitude: -100,
+          wavelength: 100,
+          segmentLength: 10
+        }
+      ], 
+  // Perform any additional initializations here
+  initialize: function (){
+     this.options.switchToSet.call(this, 'set1');
+       
+  },
+  switchToSet: function(set) {
+        this.waves = this.options[set];
+        this.options.resizeEvent.call(this);
+      },
+
+  // This function is called whenver the window is resized
+  resizeEvent: function() {
+
+    // Here is an example on how to create a gradient stroke
+    var gradient = this.ctx.createLinearGradient(0, 0, this.width, 0);
+    gradient.addColorStop(0,"rgba(255, 255, 255, 0)");
+    gradient.addColorStop(0.5,"rgba(255, 255, 255, 0.5)");
+    gradient.addColorStop(1,"rgba(255, 255, 255, 0.1)");
+
+    var index = -1;
+    var length = this.waves.length;
+      while(++index < length){
+      this.waves[index].strokeStyle = gradient;
+    }
+  }
+});
+ }
+    /*
+     * Demo of https://github.com/isuttell/sine-waves
+     */
+   
+  
+var skillsNotRenderd=true; ; 
+   var loadingScreen = pleaseWait({
+                logo: "../img/loading.png",
+                backgroundColor: '#bac8e3',
+                loadingHtml: "<div class='row animation-container'> <div class='col-xs-2 col-xs-offset-5 animation-parent '> <div class=' animation blue'></div><div class=' animation red'></div><div class=' animation green'></div></div></div> "
+               });
+ 
+
+setTimeout(function(){
+loadingScreen.finish();
+},3000);
+// initialize wow sliders 
+
+new WOW().init();
+
+
     // Offset for Main Navigation
     $('#mainNav').affix({
         offset: {
@@ -133,5 +253,6 @@ anime({
     $(window).on('resize', function() {
         $('.modal:visible').each(reposition);
     });
+
 
 })(); // End of use strict
